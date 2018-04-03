21903491c82f1cc49520ad2a3b3d5d8ada2fcf1e
linwenxiong <984329986@qq.com>
2018-04-03 22:59:17 +0800

    车位选择页面画完普通车位

---
+++ app/src/main/java/com/anyidc/cloudpark/activity/SelectUnitParkActivity.java
@@ -0,0 +1,20 @@
+package com.anyidc.cloudpark.activity;
+
+import com.anyidc.cloudpark.R;
+
+/**
+ * Created by Administrator on 2018/4/3.
+ */
+
+public class SelectUnitParkActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_select_unit_park;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
