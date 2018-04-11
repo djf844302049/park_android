2074bdf024731db7190336601146b223b115d26c
linwenxiong <984329986@qq.com>
2018-04-11 23:43:53 +0800

    选择车位（自营车场和非自营车场列表画好，还未添加点击事件）

--- app/src/main/java/com/anyidc/cloudpark/activity/SelectUnitParkActivity.java
+++ app/src/main/java/com/anyidc/cloudpark/activity/SelectUnitParkActivity.java
@@ -2,15 +2,32 @@ package com.anyidc.cloudpark.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.anyidc.cloudpark.R;
+import com.anyidc.cloudpark.adapter.ItemClickListener;
+import com.anyidc.cloudpark.adapter.ParkUnitNumAdapter;
+import com.anyidc.cloudpark.adapter.ShareParkUnitAdapter;
 import com.anyidc.cloudpark.moduel.BaseEntity;
 import com.anyidc.cloudpark.moduel.ParkDetailBean;
 import com.anyidc.cloudpark.moduel.ParkInfo;
+import com.anyidc.cloudpark.moduel.ParkUnitInfoBean;
+import com.anyidc.cloudpark.moduel.ShareParkUnitInfo;
 import com.anyidc.cloudpark.network.Api;
 import com.anyidc.cloudpark.network.RxObserver;
+import com.anyidc.cloudpark.utils.CacheData;
+import com.anyidc.cloudpark.utils.ViewUtils;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2018/4/3.
@@ -23,8 +40,15 @@ public class SelectUnitParkActivity extends BaseActivity implements View.OnClick
     private TextView tvFee;
     private TextView tvAppointment;
     private TextView tvIdAuth,tvCarAuth,tvPay;
+    private RecyclerView recyclerView;
+    private LinearLayout llImage;
 
     private ParkInfo parkInfo;
+    private ArrayList<ParkUnitInfoBean> dataList;
+    private ArrayList<ShareParkUnitInfo> shareList;
+
+    private ParkUnitNumAdapter parkUnitNumAdapter;
+    private ShareParkUnitAdapter shareParkUnitAdapter;
     public static void start(Context context,String id){
         Intent intent = new Intent(context,SelectUnitParkActivity.class);
         intent.putExtra("id",id);
@@ -48,6 +72,8 @@ public class SelectUnitParkActivity extends BaseActivity implements View.OnClick
         tvIdAuth = findViewById(R.id.tv_id_certification);
         tvCarAuth = findViewById(R.id.tv_car_certification);
         tvPay = findViewById(R.id.tv_recharge_deposit);
+        recyclerView = findViewById(R.id.rv_unit_list);
+        llImage = findViewById(R.id.ll_image);
         tvAppointment.setOnClickListener(this);
         tvIdAuth.setOnClickListener(this);
         tvCarAuth.setOnClickListener(this);
@@ -76,9 +102,36 @@ public class SelectUnitParkActivity extends BaseActivity implements View.OnClick
                     public void onSuccess(BaseEntity<ParkDetailBean> parkDetailBean) {
                         ParkDetailBean data = parkDetailBean.getData();
                         parkInfo = data.getPark();
+                        dataList = data.getList();
+                        shareList = data.getSharelist();
                         updateView();
                     }
                 });
+    }
+
+    /**
+     * 初始化实景图
+     */
+    private void initRealImage(){
+        llImage.removeAllViews();
+        String imgStr = parkInfo.getThumb();
+        String[] imgArr = imgStr.split(",");
+        if(imgArr != null && imgArr.length > 0){
+            for(String imgUrl : imgArr){
+                View view = LayoutInflater.from(this).inflate(R.layout.single_image_layout,null,false);
+                ImageView imageView = view.findViewById(R.id.iv_img);
+                ViewGroup.LayoutParams lp = imageView.getLayoutParams();
+                if(imgArr.length == 1){
+                    lp.width = ViewUtils.getWindowWidth(this);
+                }else{
+                    lp.width = ViewUtils.getWindowWidth(this) - ViewUtils.dip2px(this,30);
+                }
+                lp.height = lp.width * 9/ 16;
+                imageView.setLayoutParams(lp);
+                Glide.with(this).load(imgUrl).dontAnimate().into(imageView);
+                llImage.addView(view);
+            }
+        }
     }
 
     private void updateView(){
@@ -87,5 +140,42 @@ public class SelectUnitParkActivity extends BaseActivity implements View.OnClick
         tvAddress.setText(parkInfo.getArea_1() + " " + parkInfo.getArea_2() + " " + parkInfo.getArea_3() + " " + parkInfo.getArea_4() + " " );
         tvTotal.setText(String.valueOf(parkInfo.getNum()));
         tvFee.setText("收费标准：首" + parkInfo.getFee().getFirst_time() + "小时"+parkInfo.getFee().getMoney()+"元,之后"+parkInfo.getFee().getHourly()+"元/小时");
+        initRealImage();
+        if(parkInfo.getType() == 0){
+            llImage.setVisibility(View.GONE);
+            if(shareList == null || shareList.size() <=0 ){
+                recyclerView.setVisibility(View.GONE);
+                return;
+            }
+            recyclerView.setVisibility(View.VISIBLE);
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(linearLayoutManager);
+            shareParkUnitAdapter = new ShareParkUnitAdapter(this,shareList);
+            shareParkUnitAdapter.setOnItemClickListener(new ItemClickListener() {
+                @Override
+                public void onItemClick(View view, int position) {
+
+                }
+            });
+            recyclerView.setAdapter(shareParkUnitAdapter);
+        }else{
+            llImage.setVisibility(View.VISIBLE);
+            if(dataList == null || dataList.size() <=0 ){
+                recyclerView.setVisibility(View.GONE);
+                return;
+            }
+            recyclerView.setVisibility(View.VISIBLE);
+            GridLayoutManager gridLayoutManager = new GridLayoutManager(this,4);
+            recyclerView.setLayoutManager(gridLayoutManager);
+            parkUnitNumAdapter = new ParkUnitNumAdapter(this,dataList);
+            parkUnitNumAdapter.setOnItemClickListener(new ItemClickListener() {
+                @Override
+                public void onItemClick(View view, int position) {
+
+                }
+            });
+            recyclerView.setAdapter(parkUnitNumAdapter);
+        }
+
     }
 }
---
+++ app/src/main/java/com/anyidc/cloudpark/adapter/ShareParkUnitAdapter.java
@@ -0,0 +1,81 @@
+package com.anyidc.cloudpark.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.anyidc.cloudpark.R;
+import com.anyidc.cloudpark.moduel.ShareParkUnitInfo;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2018/4/11.
+ */
+
+public class ShareParkUnitAdapter extends RecyclerView.Adapter<ShareParkUnitAdapter.ShareParkUnitHolderView> implements View.OnClickListener{
+    private Context context;
+    private LayoutInflater mInflater;
+    private ArrayList<ShareParkUnitInfo> sharelist;
+    private ItemClickListener itemClickListener;
+    public ShareParkUnitAdapter(Context context,ArrayList<ShareParkUnitInfo> dataList){
+        this.context = context;
+        mInflater = LayoutInflater.from(context);
+        this.sharelist = dataList;
+    }
+
+
+    @Override
+    public ShareParkUnitHolderView onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.share_park_unit_item_layout,null,false);
+        return new ShareParkUnitHolderView(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ShareParkUnitHolderView holder, int position) {
+        ShareParkUnitInfo shareParkUnitInfo = sharelist.get(position);
+        if(shareParkUnitInfo != null){
+            holder.itemView.setTag(position);
+            holder.tvNum.setText(shareParkUnitInfo.getUnit_id());
+            holder.tvShareTime.setText("共享时间段："+shareParkUnitInfo.getShare_time());
+            holder.tvFee.setText("停车费：¥"+shareParkUnitInfo.getFee().getMoney());
+            holder.itemView.setOnClickListener(this);
+            holder.ivCheck.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return sharelist == null? 0 : sharelist.size();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int position = (Integer)view.getTag();
+        ShareParkUnitInfo shareParkUnitInfo = sharelist.get(position);
+        if(shareParkUnitInfo != null && shareParkUnitInfo.getStatus() == 1 && shareParkUnitInfo.getFrozen_time() == 0 && itemClickListener != null){
+            itemClickListener.onItemClick(view,position);
+        }
+    }
+
+    public void setOnItemClickListener(ItemClickListener itemClickListener){
+        this.itemClickListener = itemClickListener;
+    }
+
+    public class ShareParkUnitHolderView extends RecyclerView.ViewHolder{
+        private TextView tvNum,tvShareTime,tvFee;
+        private ImageView ivCheck;
+
+        public ShareParkUnitHolderView(View itemView){
+            super(itemView);
+            tvNum = itemView.findViewById(R.id.tv_unit_num);
+            ivCheck = itemView.findViewById(R.id.iv_check);
+            tvShareTime = itemView.findViewById(R.id.tv_share_time);
+            tvFee = itemView.findViewById(R.id.tv_unit_fee);
+        }
+    }
+}
